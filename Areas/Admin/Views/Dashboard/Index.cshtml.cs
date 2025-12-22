f928258891d133a88a34be9bd8daec00fbb18793
Yiğit Yılmaz <yigityilmaz.24207@gmail.com>
2025-12-22 18:10:25 +0300

    Add files via upload

---
+++ Areas/Admin/Views/Dashboard/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HaberPortali2.Areas.Admin.Views.Dashboard
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
