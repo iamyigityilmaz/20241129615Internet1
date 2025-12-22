d78e4f1c324deff0213166c1e1b5418d54b459f4
Yiğit Yılmaz <yigityilmaz.24207@gmail.com>
2025-12-22 21:39:16 +0300

    Add files via upload

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using HaberPortali2.Models;
+using HaberPortali2.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaberPortali2.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly IRepository<News> _newsRepo;
+
+        public HomeController(IRepository<News> newsRepo)
+        {
+            _newsRepo = newsRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var last = await _newsRepo.GetAllAsync();
+            var lastFive = last
+                .OrderByDescending(x => x.Id)
+                .Take(5);
+
+            return View(lastFive);
+        }
+    }
+}
---
+++ Areas/Admin/Views/User/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HaberPortali2.Areas.Admin.Views.User
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
