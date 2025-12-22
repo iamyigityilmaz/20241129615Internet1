b8584a054d505c35afffaefe2f2e6f4fbf0260e9
Yiğit Yılmaz <yigityilmaz.24207@gmail.com>
2025-12-22 18:11:58 +0300

    Add files via upload

---
+++ Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HaberPortali2.Models
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string FullName { get; set; } = string.Empty;
+        public bool IsAdmin { get; set; } = false;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace HaberPortali2.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+
+        public int NewsId { get; set; }
+        public News News { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
