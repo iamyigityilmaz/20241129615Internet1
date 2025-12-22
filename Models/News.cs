fa27bfd4c73ac7aa17d7ad1be32b6dc42d1008cf
Yiğit Yılmaz <yigityilmaz.24207@gmail.com>
2025-12-22 21:40:28 +0300

    Add files via upload

---
+++ Models/News.cs
@@ -0,0 +1,18 @@
+﻿namespace HaberPortali2.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string CreatedBy { get; set; } = string.Empty;
+    }
+}
