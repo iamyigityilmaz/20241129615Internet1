1b3132b3d96465c4fb7ccff10524fde2035fffe9
Yiğit Yılmaz <yigityilmaz.24207@gmail.com>
2025-12-22 21:41:22 +0300

    Add files via upload

---
+++ Repositories/IRepositories.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace HaberPortali2.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllWithIncludeAsync(
+            params Expression<Func<T, object>>[] includes
+        );
+
+        Task<T> GetByIdAsync(int id);
+        Task<T> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task SaveChangesAsync();
+    }
+}
---
+++ Views/Auth/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HaberPortali2.Views.Auth
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
